96d9b8f678bd94ab6c9fedbf1af42dd058211bd3
Nicolas2Moreau <nicolas2.moreau@mailo.com>
2021-08-31 08:06:29 +0200

    update liners

--- scriptTEST.js
+++ scriptTEST.js
@@ -1,13 +1,18 @@
-function initBtn(){
-var button = document.getElementById("btn");
-button.onclick = setStyleSheet('styleTEST.css');
-};
+// function initBtn(){
+// var button = document.getElementById("btn");
+// button.onclick = setStyleSheet('styleTEST.css');
+// };
 
 // window.button.addEventListener('click', event => {
 //     setStyleSheet('styleTESTdark.css');
 // });
 
-function setStyleSheet(url) {
+function swapCss() {
     var stylesheet = document.getElementById("css");
-    stylesheet.setAttribute('href', url);
+    var state = stylesheet.getAttribute("href");
+    if (state === "styleTEST.css") {
+        stylesheet.setAttribute('href', "styleTESTdark.css");
+    } else {
+        stylesheet.setAttribute('href', "styleTEST.css");
+    }
 }
